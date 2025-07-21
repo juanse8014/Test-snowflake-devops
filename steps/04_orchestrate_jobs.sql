80335c96c861b92f8757a5963f1534f3dd1a7cf6
Sebas Navarrete <juanse8014.jsnp@gmail.com>
2025-07-21 17:16:06 -0500

    Ajuste en Scripts Entornos de Desarrollo

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -1,5 +1,5 @@
 use role accountadmin;
-use schema quickstart_prod.gold;
+use schema quickstart_{{environment}}.gold;
 
 
 -- declarative target table of pipeline
@@ -13,7 +13,7 @@ create or alter table vacation_spots (
   , avg_cloud_cover_pct float
   , precipitation_probability_pct float
   -- STEP 5: INSERT CHANGES HERE
-) data_retention_time_in_days = 1;
+) data_retention_time_in_days = {{retention_time}};
 
 
 -- task to merge pipeline results into target table
